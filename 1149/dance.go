6e9d5d3bdcfd0b0cb23aa2c920a3be8155684da8
Eagle Chen <chygr1234@gmail.com>
2014-11-23 12:09:56 +0800

    1149 done

--- 1149/dance.go
+++ 1149/dance.go
@@ -3,10 +3,28 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strconv"
 )
 
-func a(pre string, num int) stirng {
+func a(pre string, num int) string {
+	if num == 1 {
+		return "sin(1)"
+	} else {
+		end := len(pre) - num + 1
+		result := bytes.NewBufferString(pre[:end])
+		if num%2 == 0 {
+			result.WriteString("-")
+		} else {
+			result.WriteString("+")
+		}
 
+		result.WriteString("sin(")
+		result.WriteString(strconv.Itoa(num))
+		for i := 0; i < num; i++ {
+			result.WriteString(")")
+		}
+		return result.String()
+	}
 }
 
 func s(num int) string {
@@ -17,7 +35,10 @@ func s(num int) string {
 	}
 	for i := 1; i <= num; i++ {
 		aStr = a(aStr, i)
+		result.WriteString(aStr + "+" + strconv.Itoa(num+1-i) + ")")
 	}
+	result.Truncate(result.Len() - 1)
+	return result.String()
 }
 
 func main() {
