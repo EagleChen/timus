d2f21fd9b457792e8a7d8aec1004668c87078d32
Eagle Chen <yanguangchen@vmware.com>
2014-12-05 15:51:43 +0800

    1102 passed all tests that I can find
    I suspect my code cannot handle '\r' or some invalid characters
    of the input on timus

--- 1102/dialog.go
+++ 1102/dialog.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 type Node struct {
@@ -30,7 +32,7 @@ func (node *Node) add(str string, index int, root *Node) {
 
 func buildTree() Node {
 	root := Node{make(map[byte]*Node)}
-	strs := []string{"in", "input", "one", "out", "output", "puton"}
+	strs := []string{"ni", "tupni", "eno", "tuo", "tuptuo", "notup"}
 	for _, str := range strs {
 		root.add(str, 0, &root)
 	}
@@ -39,12 +41,12 @@ func buildTree() Node {
 
 func check(root *Node, str string) bool {
 	current := root
-	for _, v := range []byte(str) {
-		if next, ok := current.children[v]; ok {
+	for i := len(str) - 1; i >= 0; i-- {
+		if next, ok := current.children[str[i]]; ok {
 			current = next
 		} else {
 			if current.isLeaf() {
-				if next, ok := root.children[v]; ok {
+				if next, ok := root.children[str[i]]; ok {
 					current = next
 				} else {
 					return false
@@ -64,13 +66,15 @@ func check(root *Node, str string) bool {
 func main() {
 	var num int
 	var str string
+	in := bufio.NewReader(os.Stdin)
 
 	fmt.Scan(&num)
 
 	root := buildTree()
 
 	for ; num > 0; num-- {
-		fmt.Scan(&str)
+		str, _ = in.ReadString('\n')
+		str = str[:len(str)-1]
 
 		if check(&root, str) {
 			fmt.Println("YES")
